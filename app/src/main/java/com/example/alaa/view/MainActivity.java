b42b4cffc53d261c0cb81a82fa2aa391d5636d74
Fateme <alavi5541@gmail.com>
2021-04-25 12:54:38 +0430

    Start Alaa test project

---
+++ app/src/main/java/com/example/alaa/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.alaa.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.alaa.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/alaa/viewmodel/ViewModel.java
@@ -0,0 +1,4 @@
+package com.example.alaa.viewmodel;
+
+public class ViewModel {
+}
